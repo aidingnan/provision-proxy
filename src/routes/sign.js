3b55db2528b9bc596f1df638e125d7c2c4631fae
CJackYang <cjackyang@qq.com>
2019-07-18 15:19:41 +0800

    fix sql bug

--- src/routes/sign.js
+++ src/routes/sign.js
@@ -5,6 +5,7 @@ const f = af => (req, res, next) => af(req, res).then(x => x, next)
 module.exports = (service) => {
   const router = Router()
   router.post('/', (req, res) => {
+    console.log(req.body)
     service.registByCsr(req.body, (err, data) => {
       err ? res.status(500).json(err) : res.status(200).json(data)
     })
